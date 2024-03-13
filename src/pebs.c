e7f74cbb90d63a2d14707d9831e3eec48306cc85
SujayYadalam94 <sujay.yadalam@gmail.com>
2024-03-13 19:15:35 +0000

    Change PEBS events based on machine

--- src/pebs.c
+++ src/pebs.c
@@ -749,6 +749,14 @@ void pebs_remove_page(struct hemem_page *page)
   pthread_mutex_unlock(&free_page_ring_lock);
 }
 
+#ifdef SCAILP
+#define L3_LOAD_MISS_LOCAL 0x2d3
+#define L3_LOAD_MISS_REMOTE 0x10d3
+#else
+#define L3_LOAD_MISS_LOCAL 0x1d3
+#define L3_LOAD_MISS_REMOTE 0x80d1
+#endif
+
 void pebs_init(void)
 {
   pthread_t kswapd_thread;
@@ -760,8 +768,8 @@ void pebs_init(void)
   for (int i = 0; i < PEBS_NPROCS; i++) {
     //perf_page[i][READ] = perf_setup(0x1cd, 0x4, i);  // MEM_TRANS_RETIRED.LOAD_LATENCY_GT_4
     //perf_page[i][READ] = perf_setup(0x81d0, 0, i);   // MEM_INST_RETIRED.ALL_LOADS
-    perf_page[i][DRAMREAD] = perf_setup(0x1d3, 0, i, DRAMREAD);      // MEM_LOAD_L3_MISS_RETIRED.LOCAL_DRAM
-    perf_page[i][NVMREAD] = perf_setup(0x80d1, 0, i, NVMREAD);     // MEM_LOAD_RETIRED.LOCAL_PMM
+    perf_page[i][DRAMREAD] = perf_setup(L3_LOAD_MISS_LOCAL, 0, i, DRAMREAD);      // MEM_LOAD_L3_MISS_RETIRED.LOCAL_DRAM
+    perf_page[i][NVMREAD] = perf_setup(L3_LOAD_MISS_REMOTE, 0, i, NVMREAD);     // MEM_LOAD_RETIRED.LOCAL_PMM
     perf_page[i][WRITE] = perf_setup(0x82d0, 0, i, WRITE);    // MEM_INST_RETIRED.ALL_STORES
     //perf_page[i][WRITE] = perf_setup(0x12d0, 0, i);   // MEM_INST_RETIRED.STLB_MISS_STORES
   }
