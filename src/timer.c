7028a0612f6e8169a22f8016369acd0dea2409fa
Konstantinos Kanellis <kkanellis@cs.wisc.edu>
2025-02-12 14:49:18 -0600

    Timer struct implementation

--- src/timer.c
+++ src/timer.c
@@ -1,10 +1,56 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/time.h>
 #include <sys/types.h>
 
 #include "timer.h"
+
+void ptimer_reset(struct ptimer *t)
+{
+  t->start.tv_sec = 0;
+  t->start.tv_usec = 0;
+  t->end.tv_sec = 0;
+  t->end.tv_usec = 0;
+  t->elapsed_us = 0;
+}
+
+void ptimer_init(struct ptimer *t, const char* prefix)
+{
+  ptimer_reset(t);
+  memset(t->prefix, 0, 64);
+  snprintf(t->prefix, 64, "%s", prefix);
+}
+
+void ptimer_start(struct ptimer *t)
+{
+  ptimer_reset(t);
+  ptimer_continue(t);
+}
+
+void ptimer_continue(struct ptimer *t)
+{
+  gettimeofday(&t->start, NULL);
+}
+
+void ptimer_stop(struct ptimer *t)
+{
+  gettimeofday(&t->end, NULL);
+  t->elapsed_us += elapsed(&t->start, &t->end) * 1000000;
+}
+
+void ptimer_print(struct ptimer *t)
+{
+  printf("[%s] elapsed: %.2lf ms\n", t->prefix, t->elapsed_us / 1000);
+}
+
+void ptimer_stop_and_print(struct ptimer *t)
+{
+  ptimer_stop(t);
+  ptimer_print(t);
+}
+
 
 /* Useful for doing arithmetic on struct timevals. M*/
 void timeDiff(struct timeval *d, struct timeval *a, struct timeval *b)
@@ -16,7 +62,6 @@ void timeDiff(struct timeval *d, struct timeval *a, struct timeval *b)
     d->tv_usec += 1000000;
   }
 }
-
 
 /* Return the no. of elapsed seconds between Starttime and Endtime. */
 double elapsed(struct timeval *starttime, struct timeval *endtime)
--- src/timer.h
+++ src/timer.h
@@ -4,6 +4,21 @@
 /* Returns the number of seconds encoded in T, a "struct timeval". */
 #define tv_to_double(t) (t.tv_sec + (t.tv_usec / 1000000.0))
 
+struct ptimer {
+  struct timeval start;
+  struct timeval end;
+  double elapsed_us;
+  char prefix[64];
+};
+
+void ptimer_init(struct ptimer *t, const char* prefix);
+void ptimer_start(struct ptimer *t);
+void ptimer_continue(struct ptimer *t);
+void ptimer_stop(struct ptimer *t);
+void ptimer_stop_and_print(struct ptimer *t);
+void ptimer_print(struct ptimer *t);
+void ptimer_reset(struct ptimer *t);
+
 void timeDiff(struct timeval *d, struct timeval *a, struct timeval *b);
 double elapsed(struct timeval *starttime, struct timeval *endtime);
 long clock_time_elapsed(struct timespec start, struct timespec end);
